5251295116acdcaa6fbd7d9a7c19a6eea0d4fc3d
Talal <82117611+talalaslam15@users.noreply.github.com>
2025-07-04 16:24:15 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-react-ts-tailwind-shadcn-template
+
+Starter template for Vite + React + TypeScript with Tailwind CSS and ShadCN UI. Preconfigured for fast project setup.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Button onClick={() => setCount((count) => count + 1)}>
+      Count is: {count}
+    </Button>
+  );
+}
+
+export default App;
