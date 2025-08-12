45325b77d2d54a98a22ce3f739d22a5228307336
Talal <talalaslam15@gmail.com>
2025-08-12 12:57:27 +0500

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -56,4 +57,4 @@ function Button({
   )
 }
 
-export { Button }
+export { Button, buttonVariants }
