fe495540328165beaae3731a2377d56adfe26ce7
Talal <talalaslam15@gmail.com>
2025-08-12 12:35:10 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  const isCI = process.env.GITHUB_ACTIONS === "true";
+  const base = process.env.BASE_PATH || (isCI ? "/expense-tracker/" : "/");
+
+  return {
+    base,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
